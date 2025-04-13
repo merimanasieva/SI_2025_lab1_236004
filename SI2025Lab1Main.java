bfadd3d380c402bd4bcddbf365d6ce309495a24d
merimanasieva <merimanasieva2004@gmail.com>
2025-04-13 10:30:24 +0200

    Implement filterByCategory

--- SI2025Lab1Main.java
+++ SI2025Lab1Main.java
@@ -85,9 +85,11 @@ class TaskManager {
 
     // 5. Filter tasks by category
     public List<Task> filterByCategory(String category) {
-        // TODO: Implement filtering logic
-        return new ArrayList<>();
-    }
+    return tasks.stream()
+                .filter(task -> task.getCategory().equalsIgnoreCase(category))
+                .collect(Collectors.toList());
+}
+
 
     // 6. Find the highest-priority unfinished task
     public List<Task> getMostUrgentTasks() {
